fee39162461ea23065cfdebeca39554b1ce88549
Eetu Mäkelä <eetu.makela@iki.fi>
2021-02-11 10:38:02 +0200

    add conda environment + Iltalehti scraper

---
+++ scrape-il.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+# %%
+
+import os
+import requests
+import logging
+import csv
+import random
+from time import sleep
+
+from typing import Any, Optional, Sequence, Dict, Callable
+import _csv
+
+logging.basicConfig(level=logging.INFO)
+# %%
+from tap import Tap
+class ArgumentParser(Tap):
+    from_date: str # from date (inclusive, YYYY-MM-DD)
+    to_date: Optional[str] = None # to date (inclusive, YYYY-MM-DD, defaults to today)
+    query: str # query string to search for (actually a stem search)
+    limit: int = 200 # number of articles to fetch per query (max==200)
+    output: str # output CSV file
+    delay: float = 1.0 # number of seconds to wait between consecutive requests
+    articles: Optional[str] = None # directory to fetch articles into (optional)
+    quiet: bool = False # Log only errors
+
+api: str = "https://api.il.fi/v1/articles/search"
+
+def main():
+    args = ArgumentParser().parse_args()
+    if args.quiet:
+        logging.basicConfig(level=logging.ERROR)
+    if args.articles is not None:
+        os.makedirs(args.articles,exist_ok=True)
+    with open(args.output,"w") as of:
+        co = csv.writer(of)
+        co.writerow(['url','title','date_created','date_modified','lead'])
+        params = {
+            'date_start':args.from_date,
+            'date_end':args.to_date,
+            'q':args.query,
+            'offset':0,
+            'limit':args.limit
+            }
+        response = requests.get(api,params)
+        r = response.json()['response']        
+        while True:
+            if r is None:
+                logging.info("Got empty repsonse for {response.url}")
+                break
+            logging.info(f"Processing {len(r)} articles from {response.url}")
+            for a in r:
+                url = 'http://iltalehti.fi/'+a['category']['category_name']+"/a/"+a['article_id']
+                title = a['title']
+                date_created = a['published_at']
+                date_modified = a['updated_at'] if a['updated_at'] is not None else date_created
+                lead = a['lead']
+                co.writerow([url,title,date_created,date_modified,lead])
+                if args.articles is not None:
+                    sleep(random.randrange(args.delay*2))
+                    file = os.path.join(args.articles,a['article_id']+".html")
+                    with open(file,"wb") as af:
+                        af.write(requests.get(url).content)
+                    logging.info(f"wrote article into {file}")
+            if len(r)!=args.limit:
+                logging.info(f"Processed {len(r)} results which is less than the limit, assuming we're done.")
+                break
+            else:
+                params['offset']+=args.limit
+                sleep(random.randrange(args.delay*2))
+                response = requests.get(api,params)
+                r = response.json()['response']
+
+
+if __name__ == '__main__':
+    main()
