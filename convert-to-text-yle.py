e322e572146ec06ec3b3f899175b426fa015473a
Pihla Toivanen <toivanenpihla@gmail.com>
2021-03-09 12:23:21 +0200

    convert html to text YLE articles

---
+++ convert-to-text-yle.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup, NavigableString
+import argparse
+import os
+import logging
+import glob
+import re
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input-directory",help="input directory",required=True)
+    parser.add_argument("-o","--output-directory",help="output directory",required=True)
+    return(parser.parse_args())
+
+def main() -> None:
+    args = parse_arguments()
+    os.makedirs(args.output_directory,exist_ok=True)
+    iglob = os.path.join(args.input_directory,"*.html")
+    for ifile in glob.glob(iglob):
+        ofile = os.path.join(args.output_directory,Path(ifile).name).replace(".html",".txt")
+        with open(ifile) as inf:
+            s = BeautifulSoup(inf,'lxml')
+            output = ""
+            headings = s.select('.yle__article__heading--h1')
+            if headings is not None:
+                for h in headings:
+                    output += h.get_text() + "\n"
+            
+            e = s.select('.yle__article__paragraph')
+            if e is not None:
+                s = e
+            
+            for r in s:
+                output += r.get_text() + "\n" 
+
+            with open(ofile,"w") as of:
+                of.write(output)
+            logging.info(f"Extracted text from {ifile} into {ofile}.")
+
+if __name__ == '__main__':
+    main()
+
