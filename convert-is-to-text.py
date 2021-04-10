34aac0645f44b20fb617cf2894e67b98ea951b96
Eetu Mäkelä <eetu.makela@helsinki.fi>
2021-04-10 18:28:52 +0300

    add article types to is text converter

--- convert-is-to-text.py
+++ convert-is-to-text.py
@@ -10,20 +10,23 @@ logging.basicConfig(level=logging.INFO)
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--input-directory",help="input directory",required=True)
+    parser.add_argument("-i","--input",help="input",required=True)
     parser.add_argument("-o","--output-directory",help="output directory",required=True)
     return(parser.parse_args())
 
 def main() -> None:
     args = parse_arguments()
     os.makedirs(args.output_directory,exist_ok=True)
-    iglob = os.path.join(args.input_directory,"*.html")
+    if os.path.isdir(args.input):
+      iglob = os.path.join(args.input,"*.html")
+    else:
+      iglob = args.input
     for ifile in glob.glob(iglob):
         ofile = os.path.join(args.output_directory,Path(ifile).name).replace(".html",".txt")
         with open(ifile) as inf:
             s = BeautifulSoup(inf,'lxml')
             output = ""
-            elements = s.select('p.article-body,.article--m-headline,.article-title-40')
+            elements = s.select('p.article-body,article.single-article,.article--m-headline,.article-title-40')
             if elements is not None:
                 for e in elements:
                     output += e.get_text() + "\n"
