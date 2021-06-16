b30b2189ed343b3a8d7b05e67bd4135ea4013769
Eetu Mäkelä <eetu.makela@iki.fi>
2021-06-16 16:39:33 +0300

    add web driver wait times as optional parameters

--- finnish_media_scrapers/fetch.py
+++ finnish_media_scrapers/fetch.py
@@ -6,37 +6,39 @@ from selenium.webdriver.common.by import By
 
 #  Scraping of the other sources can be done just using requests, but HS needs a user to be logged in, as well as renders their articles using dynamic javascript, thus requiring a Selenium session to enable fetching the articles.
 
-def prepare_session_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome,webdriver.Opera], username: str, password: str):
+def prepare_session_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome], username: str, password: str, max_web_driver_wait: int = 30):
     """Prepare a Selenium session for scraping articles from Helsingin Sanomat by logging in using the provided user id and password.
 
     Raises:
         TimeoutException: if the web driver is unable to find the elements it is looking for in 30 seconds. May indicate changes to the loging page structure.
 
     Args:
-        driver (Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome,webdriver.Opera]): the Selenium session to use
+        driver (Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome]): the Selenium session to use
         username (str): the username to log in as
         password (str): the password to use for logging in
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to render a page before failing (default: 30)
     """
     driver.get("https://www.hs.fi")
-    cookies_frame = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//iframe[@title='SP Consent Message']"))
+    cookies_frame = WebDriverWait(driver,max_web_driver_wait).until(lambda d: d.find_element(By.XPATH,"//iframe[@title='SP Consent Message']"))
     driver.switch_to.frame(cookies_frame)
-    ok_button = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//button[@title='OK']"))            
+    ok_button = WebDriverWait(driver,max_web_driver_wait).until(lambda d: d.find_element(By.XPATH,"//button[@title='OK']"))            
     ok_button.click()
     driver.switch_to.default_content()
-    login = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//*[contains(text(), 'Kirjaudu')]"))
+    login = WebDriverWait(driver,max_web_driver_wait).until(lambda d: d.find_element(By.XPATH,"//*[contains(text(), 'Kirjaudu')]"))
     driver.execute_script("arguments[0].click();", login)
-    user = WebDriverWait(driver,30).until(lambda d: d.find_element(By.ID, "username"))
+    user = WebDriverWait(driver,max_web_driver_wait).until(lambda d: d.find_element(By.ID, "username"))
     user.send_keys(username)
     pas = driver.find_element(By.ID, "password")
     pas.send_keys(password),
     pas.submit()
 
-def fetch_article_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome,webdriver.Opera],url:str) -> str:
+def fetch_article_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome], url:str, max_web_driver_wait: int = 30) -> str:
     """Fetch the HTML of a single article using a Selenium session where prepare_session_hs has been called before.
 
     Args:
-        driver (Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome,webdriver.Opera]): the Selenium session to use.
+        driver (Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome]): the Selenium session to use.
         url (str): the HS article URL to fetch article content from
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to render a page before failing (default: 30)
 
     Raises:
         ValueError: If parsing the article fails, probably due to encountering a prevously unknown layout
@@ -46,10 +48,10 @@ def fetch_article_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdriver.
     """
     driver.get(url)
     try:
-        dynamic_content = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//div[@id='page-main-content']/following-sibling::*"))
+        dynamic_content = WebDriverWait(driver,max_web_driver_wait).until(lambda d: d.find_element(By.XPATH,"//div[@id='page-main-content']/following-sibling::*"))
         if dynamic_content.tag_name == 'iframe':
             driver.switch_to.frame(dynamic_content)
-            WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH, "//div[@class='paywall-content']|//div[@id='paid-content']"))
+            WebDriverWait(driver,max_web_driver_wait).until(lambda d: d.find_element(By.XPATH, "//div[@class='paywall-content']|//div[@id='paid-content']"))
     except TimeoutException:
         raise ValueError(f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of HS articles we're not yet handling.")
     article = driver.find_element(By.XPATH,'/*').get_attribute('innerHTML')
--- finnish_media_scrapers/scripts/fetch_hs.py
+++ finnish_media_scrapers/scripts/fetch_hs.py
@@ -19,9 +19,11 @@ def parse_arguments():
   parser = argparse.ArgumentParser()
   parser.add_argument('-i','--input',help="input CSV file containing articles to fetch (from query-hs.py)",required=True)
   parser.add_argument('-o','--output',help="directory to fetch articles into",required=True)
-  parser.add_argument('-u','--username',help="email to use for article fetching")
-  parser.add_argument('-p','--password',help="password to use for article fetching")
-  parser.add_argument('-d','--delay',help="number of seconds to wait between consecutive requests",default=1.0,type=float)
+  parser.add_argument('-u','--username',help="email to use for article fetching",required=True)
+  parser.add_argument('-p','--password',help="password to use for article fetching",required=True)
+  parser.add_argument('-mw','--max-web-driver-wait',help="maximum time in seconds to wait for the webdriver to render a page before failing (default 30)",default=30,type=int)
+  parser.add_argument('-w','--web-driver',help="the Selenium web driver to use (out of remote [default],chrome,firefox)",default="remote")
+  parser.add_argument('-d','--delay',help="number of seconds to wait between consecutive requests (default 1.0)",default=1.0,type=float)
   parser.add_argument('--quiet', default=False, action='store_true', help="Log only errors")
   return(parser.parse_args())
 
@@ -30,14 +32,26 @@ def main():
     if args.quiet:
         logging.basicConfig(level=logging.ERROR)
     try:
-        driver = webdriver.Remote(desired_capabilities=webdriver.DesiredCapabilities.CHROME)
+        if args.web_driver == 'remote':
+            try:
+                driver = webdriver.Remote(desired_capabilities=webdriver.DesiredCapabilities.CHROME)
+            except:
+                logging.error("Cannot download articles if Selenium is not running on localhost. (e.g. docker run -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:4.0.0-beta-1-20210215)")
+                return
+        elif args.web_driver == 'chrome':
+            driver = webdriver.Chrome()
+        elif args.web_driver == 'firefox':
+            driver = webdriver.Firefox()
+        else:
+            logging.error("Unknown web driver specified. Must be one of remote/chrome/firefox.")
+            return
     except:
-        logging.error("Cannot download articles if Selenium is not running on localhost. (e.g. docker run -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:4.0.0-beta-1-20210215)")
+        logging.error("Couldn't initialize selected Selenium webdriver.")
         return
     try:
         logging.info("Connecting to Selenium.")
         logging.info("Logging into HS.")
-        prepare_session_hs(driver,args.username,args.password)
+        prepare_session_hs(driver,args.username,args.password,args.max_web_driver_wait)
         logging.info("Logged in.")
         sleep(3)
         os.makedirs(args.output,exist_ok=True)
@@ -47,7 +61,7 @@ def main():
                 url = a['url']
                 file = os.path.join(args.output,str(a['id'])+".html")
                 if not os.path.exists(file):
-                    article = fetch_article_hs(driver,url)
+                    article = fetch_article_hs(driver,url,args.max_web_driver_wait)
                     with open(file,"w") as af:
                         af.write("<!DOCTYPE html><head><meta charset='utf-8'></head>" + article + "</html>")
                     logging.info(f"Wrote article {url} into {file}")
