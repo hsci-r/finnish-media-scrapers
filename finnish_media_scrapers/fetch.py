b67417dd96ff09e261e6f111378c93bb897235c6
Pihla Toivanen <toivanenpihla@gmail.com>
2021-06-16 16:08:15 +0300

    Update find_element_by to find_element(By. ..

--- finnish_media_scrapers/fetch.py
+++ finnish_media_scrapers/fetch.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from typing import Union
+from selenium.webdriver.common.by import By
 
 #  Scraping of the other sources can be done just using requests, but HS needs a user to be logged in, as well as renders their articles using dynamic javascript, thus requiring a Selenium session to enable fetching the articles.
 
@@ -17,16 +18,16 @@ def prepare_session_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdrive
         password (str): the password to use for logging in
     """
     driver.get("https://www.hs.fi")
-    cookies_frame = WebDriverWait(driver,30).until(lambda d: d.find_element_by_xpath("//iframe[@title='SP Consent Message']"))
+    cookies_frame = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//iframe[@title='SP Consent Message']"))
     driver.switch_to.frame(cookies_frame)
-    ok_button = WebDriverWait(driver,30).until(lambda d: d.find_element_by_xpath("//button[@title='OK']"))
+    ok_button = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//button[@title='OK']"))            
     ok_button.click()
     driver.switch_to.default_content()
-    login = WebDriverWait(driver,30).until(lambda d: d.find_element_by_xpath("//*[contains(text(), 'Kirjaudu')]"))
+    login = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//*[contains(text(), 'Kirjaudu')]"))
     driver.execute_script("arguments[0].click();", login)
-    user = WebDriverWait(driver,30).until(lambda d: d.find_element_by_id("username"))
+    user = WebDriverWait(driver,30).until(lambda d: d.find_element(By.ID, "username"))
     user.send_keys(username)
-    pas = driver.find_element_by_id("password")
+    pas = driver.find_element(By.ID, "password")
     pas.send_keys(password),
     pas.submit()
 
@@ -45,11 +46,11 @@ def fetch_article_hs(driver: Union[webdriver.Remote,webdriver.Firefox,webdriver.
     """
     driver.get(url)
     try:
-        dynamic_content = WebDriverWait(driver,30).until(lambda d: d.find_element_by_xpath("//div[@id='page-main-content']/following-sibling::*"))
+        dynamic_content = WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH,"//div[@id='page-main-content']/following-sibling::*"))
         if dynamic_content.tag_name == 'iframe':
             driver.switch_to.frame(dynamic_content)
-            WebDriverWait(driver,30).until(lambda d: d.find_element_by_xpath("//div[@class='paywall-content']|//div[@id='paid-content']"))
+            WebDriverWait(driver,30).until(lambda d: d.find_element(By.XPATH, "//div[@class='paywall-content']|//div[@id='paid-content']"))
     except TimeoutException:
         raise ValueError(f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of HS articles we're not yet handling.")
-    article = driver.find_element_by_xpath('/*').get_attribute('innerHTML')
+    article = driver.find_element(By.XPATH,'/*').get_attribute('innerHTML')
     return(article)
