fe8f39c03db01bd08c64b016eacf2e70f05d6841
Eetu Mäkelä <eetu.makela@iki.fi>
2021-06-18 17:18:08 +0300

    fix not being able to extract certain non-paywalled feature articles

--- finnish_media_scrapers/fetch.py
+++ finnish_media_scrapers/fetch.py
@@ -1,3 +1,8 @@
+"""Utilities for fetching articles.
+Currently only affects Helsingin Sanomat. Scraping of the other sources can be done just using requests,
+but HS needs a user to be logged in, as well as renders their articles using dynamic javascript,
+thus requiring a Selenium session to enable fetching the articles.
+"""
 from typing import Union
 
 from selenium import webdriver
@@ -5,20 +10,24 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-#  Scraping of the other sources can be done just using requests, but HS needs a user to be logged in, as well as renders their articles using dynamic javascript, thus requiring a Selenium session to enable fetching the articles.
 
-
-def prepare_session_hs(driver: Union[webdriver.Remote, webdriver.Firefox, webdriver.Chrome], username: str, password: str, max_web_driver_wait: int = 30):
-    """Prepare a Selenium session for scraping articles from Helsingin Sanomat by logging in using the provided user id and password.
+def prepare_session_hs(
+        driver: Union[webdriver.Remote, webdriver.Firefox, webdriver.Chrome],
+        username: str, password: str, max_web_driver_wait: int = 30):
+    """Prepare a Selenium session for scraping articles from Helsingin Sanomat
+    by logging in using the provided user id and password.
 
     Raises:
-        TimeoutException: if the web driver is unable to find the elements it is looking for in 30 seconds. May indicate changes to the loging page structure.
+        TimeoutException: if the web driver is unable to find the elements it is looking for in 30 seconds.
+                          May indicate changes to the loging page structure.
 
     Args:
         driver (Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome]): the Selenium session to use
         username (str): the username to log in as
         password (str): the password to use for logging in
-        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to render a page before failing (default: 30)
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
     """
     driver.get("https://www.hs.fi")
     cookies_frame = WebDriverWait(driver, max_web_driver_wait).until(
@@ -35,17 +44,22 @@ def prepare_session_hs(driver: Union[webdriver.Remote, webdriver.Firefox, webdri
         lambda d: d.find_element(By.ID, "username"))
     user.send_keys(username)
     pas = driver.find_element(By.ID, "password")
-    pas.send_keys(password),
+    pas.send_keys(password)
     pas.submit()
 
 
-def fetch_article_hs(driver: Union[webdriver.Remote, webdriver.Firefox, webdriver.Chrome], url: str, max_web_driver_wait: int = 30) -> str:
-    """Fetch the HTML of a single article using a Selenium session where prepare_session_hs has been called before.
+def fetch_article_hs(
+        driver: Union[webdriver.Remote, webdriver.Firefox, webdriver.Chrome],
+        url: str, max_web_driver_wait: int = 30) -> str:
+    """Fetch the HTML of a single article using a Selenium session where
+    prepare_session_hs has been called before.
 
     Args:
         driver (Union[webdriver.Remote,webdriver.Firefox,webdriver.Chrome]): the Selenium session to use.
         url (str): the HS article URL to fetch article content from
-        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to render a page before failing (default: 30)
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
 
     Raises:
         ValueError: If parsing the article fails, probably due to encountering a prevously unknown layout
@@ -59,10 +73,12 @@ def fetch_article_hs(driver: Union[webdriver.Remote, webdriver.Firefox, webdrive
             lambda d: d.find_element(By.XPATH, "//div[@id='page-main-content']/following-sibling::*"))
         if dynamic_content.tag_name == 'iframe':
             driver.switch_to.frame(dynamic_content)
-            WebDriverWait(driver, max_web_driver_wait).until(lambda d: d.find_element(
-                By.XPATH, "//div[@class='paywall-content']|//div[@id='paid-content']"))
-    except TimeoutException:
+            if len(driver.find_elements(By.XPATH, "//div[@class='paywall-container']")) != 0:
+                WebDriverWait(driver, max_web_driver_wait).until(lambda d: d.find_element(
+                    By.XPATH, "//div[@class='paywall-content']|//div[@id='paid-content']"))
+    except TimeoutException as timeout_exception:
         raise ValueError(
-            f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of HS articles we're not yet handling.")
+            f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of HS articles we're not yet handling."
+        ) from timeout_exception
     article = driver.find_element(By.XPATH, '/*').get_attribute('innerHTML')
     return article
