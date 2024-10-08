7751992bf2274f984385f2ef44dce39ca5e24b94
shinyjack <shinnyjack@gmail.com>
2024-10-08 14:52:25 +0300

    Added AL and SK scrapers functionality

--- finnish_media_scrapers/fetch.py
+++ finnish_media_scrapers/fetch.py
@@ -1,6 +1,6 @@
 """Utilities for fetching articles.
 Currently only affects Helsingin Sanomat. Scraping of the other sources can be done just using requests,
-but HS needs a user to be logged in, as well as renders their articles using dynamic javascript,
+but HS/AL/SK needs a user to be logged in, as well as renders their articles using dynamic javascript,
 thus requiring a Selenium session to enable fetching the articles.
 """
 
@@ -41,7 +41,6 @@ async def prepare_session_hs(
     submit = await session.waitForSelector("button[type=submit]", timeout=max_web_driver_wait)
     await submit.click()
     await session.waitForNavigation()
-
 
 async def fetch_article_hs(
         session: Page,
@@ -88,3 +87,169 @@ async def fetch_article_hs(
         ) from timeout_exception
     article = await content.content()
     return article
+
+
+async def fetch_article_al(
+        session: Page,
+        url: str, max_web_driver_wait: int = 30) -> str:
+    """Fetch the HTML of a single article using a pyppeteer session where
+    prepare_session_al has been called before.
+
+    Args:
+        session (Page): the pyppeteer session to use
+        url (str): the AL article URL to fetch article content from
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
+
+    Raises:
+        ValueError: If parsing the article fails, probably due to encountering a prevously unknown layout
+
+    Returns:
+        str: the HTML of the article
+    """
+    max_web_driver_wait = 1000 * max_web_driver_wait
+    try:
+        response = await session.goto(url, timeout=max_web_driver_wait)
+        if response.status == 404:
+            raise ValueError(
+                f"The page doesn't exist for {url}."
+            )
+    except NetworkError as network_exception:
+        raise ValueError(
+            f"The page doesn't exist for {url}."
+        ) from network_exception
+    try:
+        main_content = await session.waitForXPath("//div[@id='page-main-content']/following-sibling::*", timeout=max_web_driver_wait)
+        tag_name = await (await main_content.getProperty('tagName')).jsonValue()
+        if tag_name == 'IFRAME':
+            content = await main_content.contentFrame()
+            if len(await session.xpath("//div[@class='paywall-container']")) != 0:
+                await session.waitForXPath("//div[@class='paywall-content']|//div[@id='paid-content']")
+        else:
+            content = session
+    except TimeoutError as timeout_exception:
+        raise ValueError(
+            f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of AL articles we're not yet handling."
+        ) from timeout_exception
+    article = await content.content()
+    return article
+
+
+async def prepare_session_al(
+        session: Page,
+        username: str, password: str, max_web_driver_wait: int = 30):
+    """Prepare a pyppeteer session for scraping articles from Aamulehti
+    by logging in using the provided user id and password.
+
+    Raises:
+        TimeoutError: if the web driver is unable to find the elements it is looking for in 30 seconds.
+                      May indicate changes to the loging page structure.
+
+    Args:
+        session (Page): the pyppeteer session to use
+        username (str): the username to log in as
+        password (str): the password to use for logging in
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
+    """
+    max_web_driver_wait = 1000 * max_web_driver_wait
+    await session.goto("https://www.aamulehti.fi", timeout=max_web_driver_wait)
+    cookies_frame = await session.waitForXPath("//iframe[@title='SP Consent Message']", timeout=max_web_driver_wait)
+    frame = await cookies_frame.contentFrame()
+    ok_button = await frame.waitForXPath("//button[@title='OK']", timeout=max_web_driver_wait)
+    await ok_button.click()
+    login = await session.waitForXPath("//*[contains(text(), 'Kirjaudu')]", timeout=max_web_driver_wait)
+    await login.click()
+    user = await session.waitForSelector("#username", timeout=max_web_driver_wait)
+    await user.type(username)
+    passw = await session.waitForSelector("#password", timeout=max_web_driver_wait)
+    await passw.type(password)
+    submit = await session.waitForSelector("button[type=submit]", timeout=max_web_driver_wait)
+    await submit.click()
+    await session.waitForNavigation()
+
+
+async def fetch_article_sk(
+        session: Page,
+        url: str, max_web_driver_wait: int = 30) -> str:
+    """Fetch the HTML of a single article using a pyppeteer session where
+    prepare_session_sk has been called before.
+
+    Args:
+        session (Page): the pyppeteer session to use
+        url (str): the SK article URL to fetch article content from
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
+
+    Raises:
+        ValueError: If parsing the article fails, probably due to encountering a prevously unknown layout
+
+    Returns:
+        str: the HTML of the article
+    """
+    max_web_driver_wait = 1000 * max_web_driver_wait
+    try:
+        response = await session.goto(url, timeout=max_web_driver_wait)
+        if response.status == 404:
+            raise ValueError(
+                f"The page doesn't exist for {url}."
+            )
+    except NetworkError as network_exception:
+        raise ValueError(
+            f"The page doesn't exist for {url}."
+        ) from network_exception
+    try:
+        main_content = await session.waitForXPath("//div[@id='page-main-content']/following-sibling::*", timeout=max_web_driver_wait)
+        tag_name = await (await main_content.getProperty('tagName')).jsonValue()
+        if tag_name == 'IFRAME':
+            content = await main_content.contentFrame()
+            if len(await session.xpath("//div[@class='paywall-container']")) != 0:
+                await session.waitForXPath("//div[@class='paywall-content']|//div[@id='paid-content']")
+        else:
+            content = session
+    except TimeoutError as timeout_exception:
+        raise ValueError(
+            f"Couldn't find the dynamic content I was looking for in {url}. There may be a class of AL articles we're not yet handling."
+        ) from timeout_exception
+    article = await content.content()
+    return article
+
+
+async def prepare_session_sk(
+        session: Page,
+        username: str, password: str, max_web_driver_wait: int = 30):
+    """Prepare a pyppeteer session for scraping articles from Satakunnan kansa
+    by logging in using the provided user id and password.
+
+    Raises:
+        TimeoutError: if the web driver is unable to find the elements it is looking for in 30 seconds.
+                      May indicate changes to the loging page structure.
+
+    Args:
+        session (Page): the pyppeteer session to use
+        username (str): the username to log in as
+        password (str): the password to use for logging in
+        max_web_driver_wait (int): the maximum number of seconds to wait for the webdriver to
+                                   render a page before failing (default: 30)
+
+    """
+    max_web_driver_wait = 1000 * max_web_driver_wait
+    await session.goto("https://www.satakunnankansa.fi", timeout=max_web_driver_wait)
+    cookies_frame = await session.waitForXPath("//iframe[@title='SP Consent Message']", timeout=max_web_driver_wait)
+    frame = await cookies_frame.contentFrame()
+    ok_button = await frame.waitForXPath("//button[@title='OK']", timeout=max_web_driver_wait)
+    await ok_button.click()
+    login = await session.waitForXPath("//*[contains(text(), 'Kirjaudu')]", timeout=max_web_driver_wait)
+    await login.click()
+    user = await session.waitForSelector("#username", timeout=max_web_driver_wait)
+    await user.type(username)
+    passw = await session.waitForSelector("#password", timeout=max_web_driver_wait)
+    await passw.type(password)
+    submit = await session.waitForSelector("button[type=submit]", timeout=max_web_driver_wait)
+    await submit.click()
+    await session.waitForNavigation()
+
+
---
+++ finnish_media_scrapers/scripts/htmltotext_sk.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Command-line script for converting Satakunnan kansa HTML into plain text
+"""
+
+import argparse
+import glob
+import logging
+import os
+from pathlib import Path
+
+from ..htmltotext import extract_text_from_sk_html
+
+logging.basicConfig(level=logging.INFO)
+
+
+def _parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output-directory",
+                        help="output directory", required=True)
+    parser.add_argument("input", help="input file or directory", nargs="+")
+    return parser.parse_args()
+
+
+def main():
+    args = _parse_arguments()
+    os.makedirs(args.output_directory, exist_ok=True)
+    for input_spec in args.input:
+        if os.path.isdir(input_spec):
+            input_files_glob = os.path.join(input_spec, "*.html")
+        else:
+            input_files_glob = input_spec
+        for input_file_name in glob.glob(input_files_glob):
+            output_file_name = os.path.join(args.output_directory, Path(
+                input_file_name).name).replace(".html", ".txt")
+            with open(input_file_name, encoding="utf-8") as inf:
+                try:
+                    content = extract_text_from_sk_html(inf)
+                    with open(output_file_name, "w", encoding="utf-8") as output_file:
+                        output_file.write(content)
+                    logging.info("Extracted text from %s into %s.",
+                                 input_file_name, output_file_name)
+                except ValueError:
+                    logging.error("Couldn't extract text from %s.", input_file_name)
+
+
+if __name__ == '__main__':
+    main()
